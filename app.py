82ef9945ac94f402d4605d8b88176af311c3084e
ivasonik <ivasonik@mail.ru>
2023-10-16 11:48:50 +0300

    Added connection to DB

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+
+import sqlite3
+
 from flask import Flask
+
+DB_PATH = '/home/ivasonik/MIPT/DBSM/test.db'
 
 app = Flask(__name__)
 
+def db_query(query_text):
+	with sqlite3.connect(DB_PATH) as conn:
+		cur = conn.cursor()
+		cur.execute(query_text)
+		return cur.fetchall()
 @app.route('/')
 def main_page():
-	return '<b>Hello</b>, <u>World!</u>\n'
+	return str(db_query('SELECT * FROM t1'))
 
 if __name__=='__main__':
 	app.run('0.0.0.0', debug=True)
+
