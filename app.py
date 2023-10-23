d173555e9faef645e5fe0d02247b423b92413035
ivasonik <ivasonik@mail.ru>
2023-10-23 12:25:22 +0300

    Added modification of the base.

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 
 import sqlite3
 
-from flask import Flask, render_template
+from flask import (
+    Flask, 
+    abort, redirect, render_template, request, url_for
+)
 
 DB_PATH = '/home/ivasonik/MIPT/DBSM/test.db'
 
@@ -19,10 +22,27 @@ def db_query(query_text):
 		#or instead of upper 3 strings:
 		columns = [descr[0] for descr in cur.description]
 		return columns, data
+
+def db_modify(query_text, args):
+	with sqlite3.connect(DB_PATH) as conn:
+		cur = conn.cursor()
+		cur.execute(query_text, args)
+		conn.commit()
+
 @app.route('/')
 def main_page():
 	columns, data = db_query('SELECT * FROM t1')
 	return render_template('main_page.html', rows=data, column_names = columns)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+	try:
+		a=int(request.form['a'])
+		b=request.form.get('b')
+	except (KeyError, ValueError):
+		abort(400)
+	db_modify('INSERT INTO t1(a,b) VALUES (?,?)', (a,b))
+	return redirect(url_for('main_page'))
 
 if __name__=='__main__':
 	app.run('0.0.0.0', debug=True)
